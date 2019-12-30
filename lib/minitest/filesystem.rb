5153dbe7b67089dd59759f53a23b3329153734cd
Clay Dunston <claydunston@gmail.com>
2019-12-29 19:18:32 -0600

    Start work on doc comments

--- lib/minitest/filesystem.rb
+++ lib/minitest/filesystem.rb
@@ -2,19 +2,25 @@ require "minitest/filesystem/version"
 require "minitest/filesystem/matcher"
 
 module Minitest::Assertions
+  # @return [void]
   def assert_contains_filesystem(dir, msg = nil, &block)
     matcher = Minitest::Filesystem::Matcher.new(dir, &block)
-    assert matcher.match_found?, msg || matcher.message
+    assert(matcher.match_found?, msg || matcher.message)
   end
 
+  # @return [void]
   def assert_exists(path, msg = nil, &block)
-    assert File.exist?(path), msg || "expected `#{path}` to exist, but it doesn't"
+    msg ||= "expected `#{path}` to exist, but it doesn't"
+    assert(File.exist?(path), msg)
   end
 
+  # @return [void]
   def refute_exists(path, msg = nil, &block)
-    refute File.exist?(path), msg || "expected `#{path}` not to exist, but it does"
+    msg ||= "expected `#{path}` not to exist, but it does"
+    refute(File.exist?(path), msg)
   end
 
+  # @return [void]
   def filesystem(&block)
     block
   end
--- lib/minitest/filesystem/matcher.rb
+++ lib/minitest/filesystem/matcher.rb
@@ -3,31 +3,41 @@ require "pathname"
 module Minitest
   module Filesystem
     class Matcher
+
+      # @return [void]
       def initialize(root, &block)
+        # @type [MatchingTree]
         @actual_tree = MatchingTree.new(root)
+        # @type [&block]
         @expected_tree = block
+        # @type [Boolean]
         @is_matching = true
       end
 
+      # @return [Boolean]
       def file(file)
         exists?(file, :file)
       end
 
-      def link(link, target=nil)
+      # @return [Boolean]
+      def link(link, target = nil)
         exists?(link, :symlink) && is_target_correct?(link, target)
       end
 
+      # @return [Boolean]
       def dir(dir, &block)
         matcher = self.class.new(@actual_tree.expand_path(dir), &block) if block_given?
 
         exists?(dir, :directory) && subtree(matcher)
       end
 
+      # @return [Boolean]
       def match_found?
         instance_eval(&@expected_tree)
         @is_matching
       end
 
+      # @return [String]
       def message
         @failure_msg || ""
       end
@@ -37,37 +47,49 @@ module Minitest
       # Checks existance of specified entry.
       # Existance is defined both in terms of presence and of being of the
       # right type (e.g. a file being a file and not a directory)
-      def exists?(entry, kind=:entry)
+      #
+      # @return [Boolean]
+      def exists?(entry, kind = :entry)
         entry(entry, kind) && is_a?(entry, kind)
       end
 
       # Checks if an entry with given name exists.
-      def entry(entry, kind=:entry)
+      #
+      # @return [Boolean]
+      def entry(entry, kind = :entry)
         update_matching_status(
           @actual_tree.include?(entry),
-          not_found_msg_for(entry, kind))
+          not_found_msg_for(entry, kind),
+        )
       end
 
       # Checks if a specific entry (supposed to exist) is of a given kind.
+      # @return [Boolean]
       def is_a?(entry, kind)
         update_matching_status(
           @actual_tree.is_a?(entry, kind),
-          mismatch_msg_for(entry, kind))
+          mismatch_msg_for(entry, kind),
+        )
       end
 
       # Checks the target of a symbolic link.
+      #
+      # @return [Boolean]
       def is_target_correct?(link, target)
         return true unless target
 
         update_matching_status(
           @actual_tree.has_target?(link, target),
-          link_target_mismatch_msg_for(link, target))
+          link_target_mismatch_msg_for(link, target),
+        )
       end
 
+      # @return [Boolean,nil]
       def subtree(matcher)
         update_matching_status(matcher.match_found?, matcher.message) if matcher
       end
 
+      # @return [Boolean]
       def update_matching_status(check, msg)
         @is_matching = @is_matching && check
         set_failure_msg(msg) unless @is_matching
@@ -75,56 +97,69 @@ module Minitest
         @is_matching
       end
 
+      # @return [String]
       def not_found_msg_for(entry, kind)
         "Expected `#{@actual_tree.root}` to contain #{kind} `#{entry}`."
       end
 
+      # @return [String]
       def mismatch_msg_for(entry, kind)
         "Expected `#{entry}` to be a #{kind}, but it was not."
       end
 
+      # @return [String]
       def link_target_mismatch_msg_for(link, target)
         "Expected `#{link}` to point to `#{target}`, but it pointed to #{@actual_tree.follow_link(link)}"
       end
 
+      # @return [void]
       def set_failure_msg(msg)
         @failure_msg ||= msg
       end
 
       class MatchingTree
+        # @return [Pathname]
         attr_reader :root
 
+        # @return [void]
         def initialize(root)
           @root = Pathname.new(root)
           @tree = expand_tree_under @root
         end
 
+        # @return [Boolean]
         def include?(entry)
           @tree.include?(expand_path(entry))
         end
 
+        # @return [Boolean]
         def is_a?(entry, kind)
           (expand_path entry).send("#{kind}?")
         end
-        
+
+        # @return [Boolean]
         def has_target?(entry, target)
           expand_path(target) == follow_link(entry)
         end
 
+        # @return [Pathname]
         def expand_path(file)
           @root + Pathname.new(file)
         end
 
+        # @return [Pathname]
         def follow_link(link)
           Pathname.new(File.readlink(expand_path(link)))
         end
 
         private
-        
+
+        # @return [Array<Pathname>]
         def expand_tree_under(dir)
           Pathname.glob(dir.join("**/*"))
         end
       end
+
     end
   end
 end
